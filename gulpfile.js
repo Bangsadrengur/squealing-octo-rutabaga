5c4f06c93c9d370f4572ee1eb6f0e709b10fc0df
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2015-10-11 15:05:07 +0000

    Added a step to compile a dist folder (fullblown runtime) to gulp called build. Added a step to cp src/ to dist/. Added a step to browserify mithril.js from node_modules to dist. Added commands clean, dist, watch-html and start to npm command. Added missing html5-linting dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var html5Lint = require('gulp-html5-lint');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var del = require('del');
 
 gulp.task('html5-lint', function() {
   return gulp.src('./src/*.html')
@@ -9,6 +12,24 @@ gulp.task('html5-lint', function() {
 gulp.task('watch-html', function() {
   gulp.watch('src/*.html', ['html5-lint']);
 });
+
+gulp.task('mithril', function() {
+  return browserify('./node_modules/mithril/mithril.min.js')
+    .bundle()
+    .pipe(source('mithril.min.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('srcToDist', function() {
+  gulp.src('./src/**')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function() {
+  return del(['./dist']);
+});
+
+gulp.task('build', ['srcToDist', 'mithril']);
 
 gulp.task('default', function() {
 });
